7adf1e517d7ffbd5953bed7c3dc715207434b888
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 08:55:46 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//alert ('Mi archivo está bien linkeado');
+
+//Cantidad de caracteres
+
+//Crear un programa que pida al usuario ingresar un texto y muestre un mensaje con la cantidad de caracteres que tiene ese texto. INVESTIGAR CÓMO HACERLO
+
+const texto = prompt ('Ingrese un texto al contador de caracteres');
+const cantidadDeCaracteres = texto.length;
+
+alert(`El texto ingresado contiene ${cantidadDeCaracteres} caracteres.`);
